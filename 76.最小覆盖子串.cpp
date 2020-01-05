9f8c353fd1447e030956f186f846ac16341c5b35
lixuefeng <loooo139@gmail.com>
2020-01-05 17:08:43 +0800

    update

--- 76.最小覆盖子串.cpp
+++ 76.最小覆盖子串.cpp
@@ -8,22 +8,6 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // vector<int>map(256,0);
-        // int cnt=0;
-        // for(auto i:t){
-        //     if(map[i]==0)cnt++;
-        //     map[i]++;
-        // }
-        // string res="";
-        // for(int i=0,j=0,c=0;i<s.size();i++){
-        //     if(map[s[i]]==1)c++;
-        //     map[s[i]]--;
-        //     while(c==cnt&&map[s[j]]<0)map[s[j++]]++;
-        //     if(c==cnt){
-        //         if(res.empty()||res.size()>i-j+1)
-        //             res = s.substr(j, i - j + 1);
-        //     }
-        // }
         vector<int>map(256,0);
         int cnt=0;
         for(auto i:t){
@@ -34,10 +18,10 @@ public:
         for(int i=0,j=0,c=0;i<s.size();i++){
             if(map[s[i]]==1)c++;
             map[s[i]]--;
-            while(map[s[j]]<0)map[s[j++]]++;
-            if(cnt==c){
+            while(c==cnt&&map[s[j]]<0)map[s[j++]]++;
+            if(c==cnt){
                 if(res.empty()||res.size()>i-j+1)
-                res=s.substr(j,i-j+1);
+                    res = s.substr(j, i - j + 1);
             }
         }
         return res;
